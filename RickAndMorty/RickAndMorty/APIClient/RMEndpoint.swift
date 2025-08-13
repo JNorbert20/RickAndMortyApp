22852622da43fdb4280a34f5719f5110bb716184
Jaczina Norbert <apple@MacBookPro.home>
2025-08-13 16:11:00 +0200

    Added docstrings and API related files to the project

---
+++ RickAndMorty/RickAndMorty/APIClient/RMEndpoint.swift
@@ -0,0 +1,14 @@
+//
+//  RMEndpoint.swift
+//  RickAndMorty
+//
+//  Created by Jaczina Norbert on 2025. 08. 13..
+//
+
+import Foundation
+
+@frozen enum RMEndpoint: String {
+    case character
+    case location
+    case episode
+}
---
+++ RickAndMorty/RickAndMorty/APIClient/RMRequest.swift
@@ -0,0 +1,12 @@
+//
+//  RMRequest.swift
+//  RickAndMorty
+//
+//  Created by Jaczina Norbert on 2025. 08. 13..
+//
+
+import Foundation
+
+final class RMRequest{
+    
+}
